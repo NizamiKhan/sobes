8dbb3e3724211f1f0a9856d4276ef1406f27727b
NizamiKhan <sauron270695@yandex.ru>
2017-11-19 20:17:42 +0300

    add method fu1 and fix

--- patterns/DI/DI.php
+++ patterns/DI/DI.php
@@ -8,6 +8,10 @@ class Car {
     public function __constructor(IEngine $engine) {
         $this->engine = $engine;
     }
+
+    public function fu1(){
+        echo __METHOD__;
+    }
 }
 
-$car = new Car(new V8Engine());
+//$car = new Car(new V8Engine());
